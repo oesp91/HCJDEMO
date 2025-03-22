3d48e76614579c99910a130726c40e777308532e
mabbak5 <mabbak66@gmail.com>
2025-03-22 23:26:05 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function changeBackgroundColor(color) {
+  document.body.style.backgroundColor = color
+}
+function pink() {
+  changeBackgroundColor('pink')
+}
+function lightblue() {
+  changeBackgroundColor('lightblue')
+}
+function yellow() {
+  changeBackgroundColor('yellow')
+}
+function reset() {
+  changeBackgroundColor('white')
+}
